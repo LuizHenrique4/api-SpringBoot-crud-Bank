7070b153e17f4999d763824145de9cc0dbf23a0d
Luiz Henrique <luiz.paz@bandtec.com.br>
2022-08-13 23:35:12 -0300

    FEAT(servives): Adicionando servicos adjacentes

---
+++ api-SpringBoot-crud-Bank/src/test/kotlin/valemobi/apiSpringBootcrudBank/ApiSpringBootCrudBankApplicationTests.kt
@@ -0,0 +1,13 @@
+package valemobi.apiSpringBootcrudBank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiSpringBootCrudBankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
