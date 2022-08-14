3da5fb46dbcf8fae4cfa6fa3ea148464c3050266
Luiz Henrique <luiz.paz@bandtec.com.br>
2022-08-13 23:31:15 -0300

    (AccountController.kt): Adicionando endpoints create, getById, Delete, Update

---
+++ api-SpringBoot-crud-Bank/src/main/kotlin/valemobi/apiSpringBootcrudBank/ApiSpringBootCrudBankApplication.kt
@@ -0,0 +1,11 @@
+package valemobi.apiSpringBootcrudBank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiSpringBootCrudBankApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiSpringBootCrudBankApplication>(*args)
+}
